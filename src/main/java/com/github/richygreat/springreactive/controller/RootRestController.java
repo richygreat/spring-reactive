e9d7be10cfef5b5a04e2d998ce66378855aade3b
rkalingarayar <richy.lourdusamy-kalingarayar@smava.de>
2019-05-13 15:21:03 +0200

    Logs and Mono

--- src/main/java/com/github/richygreat/springreactive/controller/RootRestController.java
+++ src/main/java/com/github/richygreat/springreactive/controller/RootRestController.java
@@ -4,10 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import reactor.core.publisher.Mono;
+
 @RestController
 public class RootRestController {
 	@GetMapping("/{message}")
 	public String echo(@PathVariable("message") String message) {
+		System.out.println("echo: message: " + message);
 		return message;
+	}
+
+	@GetMapping("/mono/{message}")
+	public Mono<String> echoMono(@PathVariable("message") String message) {
+		return Mono.just(message);
 	}
 }
